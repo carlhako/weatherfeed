b28204c5ac21f53ef12b01a13b7c262eea98b8b6
carlhako <carlhako@gmail.com>
2015-02-28 11:54:36 +1000

    adding cors support back in

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
